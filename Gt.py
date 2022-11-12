be0ea5e011c639443db032ab7be49ab3b89ad86e
unknown <gdtorres11@gmail.com>
2022-11-12 09:11:56 +0800

    Create Gt.py

---
+++ Gt.py
@@ -0,0 +1,27 @@
+#Tuazon, Kristine Faith G.
+
+# Ask for the name of the user
+# Print the name of the user in all capital letters for example "Hello USER!"
+# Take the 5 numbers from user.
+# Using result as the variable perform the following equation: num1**num2/num3+num4%num5
+# For example, if the computed result is 12.66667 when printed out, the format should be 12.67
+# Print out the result of the equation. Follow the sample output: "Result is: 12.67"
+# If the result is equal to or above 100, print out "Yahoo!" else, print out "Yehey!"
+# Comment your name at the beginning of your program.
+# Save your work as Prelim.py
+# Upload your file here.
+
+
+
+user_name = input("Enter your name")
+print ("Hello USER!")
+num1 = int(input("Give a number: "))
+num2 = float(input(" Give another number: "))
+num3 = int(input(" Give another number: "))
+num4 = int(input(" Give another number: "))
+num5 = int(input(" Give another number: "))
+
+formula = num1**num2/num3+num4%num4
+print (formula)
+
+
